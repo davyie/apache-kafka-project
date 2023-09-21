d1994302803bd994f25ccb1da112d741d64f9d34
David Yu <yu.davidcheng@gmail.com>
2023-09-21 17:25:49 +0200

    Use the producers

--- kafka-app/src/main/java/org/example/App.java
+++ kafka-app/src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        SampleProducer sampleProducer = new SampleProducer();
+    public static void main( String[] args ) throws IOException {
+//        SampleProducer sampleProducer = new SampleProducer();
+        StockProducer stockProducer = new StockProducer();
     }
 }
