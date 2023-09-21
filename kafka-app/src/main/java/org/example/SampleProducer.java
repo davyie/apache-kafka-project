b16398b36834b352bcbf46c0d4c10d16e5105b0f
David Yu <yu.davidcheng@gmail.com>
2023-09-21 09:56:57 +0200

    Add sample program to produce to Kafka

---
+++ kafka-app/src/main/java/org/example/SampleProducer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SampleProducer {
+    public SampleProducer() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        ProducerRecord producerRecord = new ProducerRecord("channel", "name", "This is awesome!");
+
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.close();
+    }
+}
